bd8324f2895e25ada23242dc1ae598f26992edb9
Artem Rodygin <artem.rodygin@gmail.com>
2023-11-14 22:53:03 +1300

    Use raw string literals in migrations

--- Dal/Migrations/001_InitialMigration.cs
+++ Dal/Migrations/001_InitialMigration.cs
@@ -11,34 +11,34 @@ public sealed class InitialMigration : AbstractMigration
 {
     protected override string GetUpSql(IServiceProvider services)
     {
-        return @"
-            create table ""ToDos"" (
-                ""Id"" serial primary key,
-                ""Description"" text not null,
-                ""Complete"" boolean not null default false
+        return """
+            create table "ToDos" (
+                "Id" serial primary key,
+                "Description" text not null,
+                "Complete" boolean not null default false
             );
 
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Application Startup', true);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Kestrel & HTTP Context', true);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Dependency Injection', true);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Middlewares & Filters', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Configuration & Deployment', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Routing & Controllers', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Model Binding & Validation', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Multi-threading', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Entity Framework Core', true);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Background Tasks', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Authentication & Authorization', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Logging & Tracing', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Unit Testing & Code Coverage', false);
-            insert into ""ToDos"" (""Description"", ""Complete"") values ('Code Style & Static Analysis', false);
-        ";
+            insert into "ToDos" ("Description", "Complete") values ('Application Startup', true);
+            insert into "ToDos" ("Description", "Complete") values ('Kestrel & HTTP Context', true);
+            insert into "ToDos" ("Description", "Complete") values ('Dependency Injection', true);
+            insert into "ToDos" ("Description", "Complete") values ('Middlewares & Filters', false);
+            insert into "ToDos" ("Description", "Complete") values ('Configuration & Deployment', false);
+            insert into "ToDos" ("Description", "Complete") values ('Routing & Controllers', false);
+            insert into "ToDos" ("Description", "Complete") values ('Model Binding & Validation', false);
+            insert into "ToDos" ("Description", "Complete") values ('Multi-threading', false);
+            insert into "ToDos" ("Description", "Complete") values ('Entity Framework Core', true);
+            insert into "ToDos" ("Description", "Complete") values ('Background Tasks', false);
+            insert into "ToDos" ("Description", "Complete") values ('Authentication & Authorization', false);
+            insert into "ToDos" ("Description", "Complete") values ('Logging & Tracing', false);
+            insert into "ToDos" ("Description", "Complete") values ('Unit Testing & Code Coverage', false);
+            insert into "ToDos" ("Description", "Complete") values ('Code Style & Static Analysis', false);
+        """;
     }
 
     protected override string GetDownSql(IServiceProvider services)
     {
-        return @"
-            drop table if exists ""ToDos"";
-        ";
+        return """
+            drop table if exists "ToDos";
+        """;
     }
 }
