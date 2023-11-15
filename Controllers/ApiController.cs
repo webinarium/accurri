37df404a64fd21e25260ca9879abd54f90ffec7c
Artem Rodygin <artem.rodygin@gmail.com>
2023-11-15 16:37:56 +1300

    API

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Accurri.Controllers;
 
+/// <summary>
+/// API for ToDo resources.
+/// </summary>
 [ApiController]
 [Route("/todo")]
 public sealed class ApiController : Controller
@@ -16,13 +19,20 @@ public sealed class ApiController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly IUnitOfWork _unitOfWork;
 
+    /// <summary>
+    /// Dependency injection.
+    /// </summary>
     public ApiController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
         _unitOfWork = unitOfWork;
     }
 
+    /// <summary>
+    /// Returns list of existing resources.
+    /// </summary>
     [HttpGet("")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ICollection<ToDo>))]
     public IActionResult List()
     {
         _logger.LogInformation("GET /todo");
@@ -34,7 +44,13 @@ public sealed class ApiController : Controller
         return Ok(todos);
     }
 
+    /// <summary>
+    /// Creates new resource.
+    /// </summary>
+    /// <response code="201">Returns the newly created resource.</response>
     [HttpPost("")]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ToDo))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Add(AddModel model)
     {
         _logger.LogInformation("POST /todo");
@@ -50,10 +66,16 @@ public sealed class ApiController : Controller
 
         _logger.LogDebug("Created new todo with ID={id}", todo.Id);
 
-        return Created(string.Empty, todo.Id.ToString());
+        return Created(string.Empty, todo);
     }
 
+    /// <summary>
+    /// Returns specified resource.
+    /// </summary>
+    /// <param name="id"></param>
     [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ToDo))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get(int id)
     {
         _logger.LogInformation($"GET /todo/{id}");
@@ -72,7 +94,15 @@ public sealed class ApiController : Controller
         return Ok(todo);
     }
 
+    /// <summary>
+    /// Updates specified resource.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="model"></param>
     [HttpPut("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update(int id, UpdateModel model)
     {
         _logger.LogInformation($"PUT /todo/{id}");
@@ -97,7 +127,12 @@ public sealed class ApiController : Controller
         return Ok();
     }
 
+    /// <summary>
+    /// Deletes specified resource.
+    /// </summary>
+    /// <param name="id"></param>
     [HttpDelete("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> Remove(int id)
     {
         _logger.LogInformation($"DELETE /todo/{id}");
